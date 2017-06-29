72e97ffabd234cea285670145ef58422cec036f9
仁木美来 <meirai@nikimikunoMacBook-Pro.local>
2017-06-30 00:40:26 +0900

    初期値を変更してなんども計算

--- heuristic2.java
+++ heuristic2.java
@@ -6,12 +6,15 @@ class heuristic2{
     static ArrayList<Integer> xPos = new ArrayList<Integer>();
     static ArrayList<Integer> yPos = new ArrayList<Integer>();
     static ArrayList<Integer> order = new ArrayList<Integer>();
+    static ArrayList<Integer> bestOrder = new ArrayList<Integer>();
     static ArrayList<Integer> reverseOrder = new ArrayList<Integer>();
     static ArrayList<Integer> distance = new ArrayList<Integer>();
     static ArrayList<Integer> leftPoints = new ArrayList<Integer>();
     static int startPoint = 0;
     static int xMaxPoint = 0;
     static int yMaxPoint = 0;
+    static double len = 0;
+    static double bestLength = 0;
 
     public static void readCSVFile(String fileName){
         try{
@@ -45,11 +48,8 @@ class heuristic2{
             FileWriter fw = new FileWriter(fileName, false);
             PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
             pw.println("index");
-            for(int i = 0; i < order.size(); i++){
-                pw.println(order.get(i));
-            }
-            for(int i = reverseOrder.size() - 1; i >= 0; i--){
-                pw.println(reverseOrder.get(i));
+            for(int i = 0; i < bestOrder.size(); i++){
+                pw.println(bestOrder.get(i));
             }
             pw.close();
         }catch (IOException ex){
@@ -58,7 +58,7 @@ class heuristic2{
     }
 
     //xが小さいものをstartPointとした
-    public static void getStartPoint(){
+    public static void getStartSmallPoint(){
         for(int i = 0; i < xPos.size(); i++){
             int x = xPos.get(i);
             int startX = xPos.get(startPoint);
@@ -73,6 +73,28 @@ class heuristic2{
             }
         }
     }
+
+    public static void getStartMidiamPoint(){
+        for(int i = 0; i < xPos.size(); i++){
+            int x = xPos.get(i);
+            int startX = xPos.get(startPoint);
+            int maxX = xPos.get(xMaxPoint);
+            if(maxX/2 - startX > maxX/2 - x){
+                startPoint = i;
+            }else if(startX == x){
+                int y = yPos.get(i);
+                int startY = yPos.get(startPoint);
+                if(startY > y){
+                    startPoint = i;
+                }
+            }
+        }
+    }
+
+    public static void getStartRndPoint(int rnd){
+        startPoint = rnd;
+    }
+
 
     public static void getXMaxPoint(){
         for(int i = 0; i < xPos.size(); i++){
@@ -120,6 +142,8 @@ class heuristic2{
     }
 
     public static void calcOrder(){
+        order.clear();
+        reverseOrder.clear();
         initLeftPoints();
         getDistance(startPoint);
         //testGetDistance();
@@ -127,15 +151,19 @@ class heuristic2{
         while(leftPoints.size() != 0){
             int headPoint = order.get(order.size() - 1);
             int endPoint = reverseOrder.get(reverseOrder.size() - 1);
-            System.out.println("headpoint : " + headPoint + " ,endpoint : " + endPoint);
+            //System.out.println("headpoint : " + headPoint + " ,endpoint : " + endPoint);
             getNextPoints(headPoint, endPoint);
         }
+        for(int i = reverseOrder.size() - 1; i >= 0 ; i--){
+            order.add(reverseOrder.get(i));
+        }        
+
     }
 
     public static void testLeftPoints(){
         System.out.println("");
         for(int i = 0; i < leftPoints.size(); i++){
-            System.out.print("leftPoints :" + leftPoints.get(i));
+            //System.out.print("leftPoints :" + leftPoints.get(i));
         }
     }
 
@@ -147,10 +175,8 @@ class heuristic2{
         if(leftPoints.size() != 0){
             nextEnd = searchPoint(end);
             leftPoints.remove((Integer)nextEnd);
-            System.out.println("in");
-            
         }
-        System.out.println("nextHead : " + nextHead + " ,nextEnd : " + nextEnd);
+        //System.out.println("nextHead : " + nextHead + " ,nextEnd : " + nextEnd);
         if(leftPoints.size() == 0){
             order.add(nextHead);
         }else if(ifCross(head, nextHead, end, nextEnd)){
@@ -186,16 +212,12 @@ class heuristic2{
                 order.add(min);
                 
                 leftPoints.remove((Integer)min);
-                if(cnt == 2)
-                    System.out.println("order[1] = " + min);
                 cnt++;
                 
             }else{
                 reverseOrder.add(min);
                 
                 leftPoints.remove((Integer)min);
-                if(cnt == 1)
-                    System.out.println("reverseOrder[0] = " + min);
                 cnt++;
                 
             }   
@@ -217,10 +239,35 @@ class heuristic2{
         for(int i = 0; i < order.size(); i++){
             System.out.println(order.get(i));
         }
-        for(int i = reverseOrder.size() - 1; i >= 0 ; i--){
-            System.out.println(reverseOrder.get(i));
+    }
+
+    public static void getLength(){
+        int end = order.size() - 1;
+        double edx = xPos.get(order.get(end))-xPos.get(order.get(0));
+        double edy = yPos.get(order.get(end))-yPos.get(order.get(0));
+        len = Math.sqrt( edx*edx + edy*edy );
+        for(int i = 1; i < order.size(); i++){
+            int current = order.get(i-1);
+            int next = order.get(i);
+            double dx = xPos.get(current)-xPos.get(next);
+            double dy = yPos.get(current)-yPos.get(next);
+            len += Math.sqrt( dx*dx + dy*dy );
         }
     }
+
+    public static void printLength(){
+        System.out.println(len);
+    }
+
+    public static void copyOrder(){
+        bestOrder.clear();
+        for(int i = 0; i < order.size(); i++){
+            bestOrder.add(order.get(i));
+        }
+    }
+
+
+
 
     public static void main(String args[]){
         if(args.length != 2){
@@ -228,12 +275,46 @@ class heuristic2{
             System.exit(1);
         }
         readCSVFile(args[0]);
-        getStartPoint();
         getXMaxPoint();
         getYMaxPoint();
 
+        getStartSmallPoint();
         calcOrder();
-        testCalcOrder();
+        getLength();
+        //printLength();
+
+        bestLength = len;
+        copyOrder();
+        //testCalcOrder();
+        getStartMidiamPoint();
+        calcOrder();
+        getLength();
+        //printLength();
+
+        if(bestLength > len){
+            bestLength = len;
+            copyOrder();
+        }
+        //testCalcOrder();
+
+        long seed = 1L;
+        Random r = new Random(seed);
+        int cnt = 0;
+        while(cnt <10){
+            int rnd = r.nextInt(xPos.size());
+            getStartRndPoint(rnd);
+            calcOrder();
+            getLength();
+            //printLength();
+            if(bestLength > len){
+                bestLength = len;
+                copyOrder();
+            }
+            cnt++;
+        }
+
+        //testCalcOrder();
+        System.out.println("length: " + bestLength);
         writeCSVFile(args[1]);   
 
         
